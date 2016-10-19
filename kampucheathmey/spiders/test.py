faf80ec786512cc54068617999eab71078a8b0ac
Vicheanak <vicheanak@gmail.com>
2016-10-19 14:51:15 +0700

    auto-bash

--- kampucheathmey/spiders/test.py
+++ kampucheathmey/spiders/test.py
@@ -4,13 +4,15 @@ from scrapy.spiders import CrawlSpider, Rule
 from kampucheathmey.items import KampucheathmeyItem
 from scrapy.linkextractors import LinkExtractor
 import time
+import lxml.etree
+import lxml.html
 
 
 class TestSpider(CrawlSpider):
     name = "kampucheathmey"
     allowed_domains = ["kampucheathmey.com"]
     start_urls = [
-    'https://kampucheathmey.com/2016/ព័ត៌មានជាតិ/ព័ត៌មានជាតិទូទៅ',
+    'http://kampucheathmey.com/2016/ព័ត៌មានជាតិ/ព័ត៌មានជាតិទូទៅ',
     ]
 
     def parse(self, response):
@@ -40,35 +42,34 @@ class TestSpider(CrawlSpider):
             else:
                 item['description'] = description.extract_first()
 
-            imageUrl = article.xpath("""
-                div[@class="image_post feature-item loadmore_list_image"]/a[1]/img[1]/@src
-                """)
 
-            item['imageUrl']
-            if not imageUrl:
-                print('Kampucheathmey => [' + now + '] No imageUrl')
-            else:
-                item['imageUrl'] = imageUrl.extract_first()
-
-            yield item
+            request = scrapy.Request(item['url'], callback=self.parse_detail)
+            request.meta['item'] = item
+            yield request
 
     def parse_detail(self, response):
         item = response.meta['item']
         hxs = scrapy.Selector(response)
         now = time.strftime('%Y-%m-%d %H:%M:%S')
 
-        item_page = hxs.css('div.item-page')
-        description = item_page.xpath('p[1]/text()')
-        if not description:
-            print('Kampucheathmey => [' + now + '] No description')
-        else:
-            item['description'] = item_page.xpath('p[1]/strong/text()').extract_first() + ' ' + description.extract_first()
 
-        imageUrl = item_page.xpath('p[last()]/img/@src')
+        imageUrl = hxs.xpath("""
+            //div[@class="single_post_format_image"]/img[1]/@src
+            """)
+        item['imageUrl'] = ''
         if not imageUrl:
             print('Kampucheathmey => [' + now + '] No imageUrl')
         else:
             item['imageUrl'] = imageUrl.extract_first()
+
+        root = lxml.html.fromstring(response.body)
+        lxml.etree.strip_elements(root, lxml.etree.Comment, "script", "head")
+        htmlcontent = ''
+
+        for p in root.xpath('//div[@class="post_content"][1]'):
+            htmlcontent = lxml.html.tostring(p, pretty_print=True, encoding=unicode)
+
+        item['htmlcontent'] = htmlcontent
 
 
         yield item
